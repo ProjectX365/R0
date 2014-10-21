0d21837a34b85ffd9d2ea823fb1af39b7a9e6bab
Ujwalmanandhar <ujwalmanandhar@gmail.com>
2014-10-20 17:21:08 -0700

    Initial Draft
    Initial Draft for UI

---
+++ UI/FundRaiser.UI.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FundRaiser.UI.Web.Startup))]
+namespace FundRaiser.UI.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
